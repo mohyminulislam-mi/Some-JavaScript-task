73447f7d33aa5935dd8b25a0e9e3b8f32d834407
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-08-04 23:54:17 +0600

    001-JavaScript-Task is solved :)

---
+++ 001-JavaScript-Task.js
@@ -0,0 +1,5 @@
+/** Problem -001 ( Divide the Asset ) */
+var area = 800; //Your value, (If need you can change here)
+//write your code here
+var equalShare = area /2; //Simple division the input value
+console.log( equalShare)
