2d2f8cebe11a5db63fc97967f592894a27d54957
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-08-04 23:57:07 +0600

    002-JavaScript-Task is solved :)

---
+++ 002-JavaScript-Task.js
@@ -0,0 +1,16 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+var laptop = 25000;
+var cycle = 10000;
+
+if ( money >= 25000){
+   console.log("Laptop")
+}
+else if ( money >= 10000){
+   console.log("Cycle")
+}
+else {
+   console.log("Chocolate")
+}
